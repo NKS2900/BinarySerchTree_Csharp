ab3892c56aaba18844deac1854463c4d98d7636c
NIJMA SAYYAD <nksayyad2900@outlook.com>
2020-12-18 20:22:05 +0530

    [NIJAM] Add:Binary Search Tree in CSharp.

---
+++ BinarySerchTree_Csharp/BinarySearchTree.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySerchTree_Csharp
+{
+    class BinarySearchTree<T> where T : IComparable
+    {
+        
+    }
+}
---
+++ BinarySerchTree_Csharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BinarySerchTree_Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Binary_Search_Tree***");
+            
+        }
+    }
+}
